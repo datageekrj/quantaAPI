1aff9467fb8452f37529d686468e94adaebf1797
datageekrj <55853415+datageekrj@users.noreply.github.com>
2024-12-18 17:09:55 +0530

    Update webflowApiFrontend.js

--- webflowApiFrontend.js
+++ webflowApiFrontend.js
@@ -3,6 +3,8 @@ let chat = null;
 let user_id = null;
 let is_user_verified = false;
 
+alert("Hey there");
+
 let ids = ["Catan_Special_Number","Expected_Num_Boxes_with_Coupons", "Test_Problem"];
 
 const dataPromise = fetch("https://quantaapi-pw5v.onrender.com/getProblems", {
