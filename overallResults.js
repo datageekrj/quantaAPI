4be3cb85cb0abc50c51ee9ac0d2f571dfb5943b3
datageekrj <55853415+datageekrj@users.noreply.github.com>
2024-12-20 19:26:46 +0530

    Update overallResults.js

--- overallResults.js
+++ overallResults.js
@@ -5,6 +5,24 @@ window.MathJax = {
     },
 };
 
+const script1 = document.createElement("script");
+script1.src = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-mml-chtml.min.js";
+script1.async = true;
+const script2 = document.createElement("script");
+script2.src = "https://cdn.jsdelivr.net/npm/marked/marked.min.js";
+script2.async = true;
+const script3 = document.createElement("script");
+script3.src = "https://cdn.jsdelivr.net/npm/katex/dist/katex.min.js";
+script3.async = true;
+document.head.appendChild(script3);
+document.head.appendChild(script2);
+document.head.appendChild(script1);
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "https://cdn.jsdelivr.net/npm/katex/dist/katex.min.css";
+document.head.appendChild(link);
+
+
 let serverLink = 'https://quantaapi-pw5v.onrender.com/';
 let popup = null;
 let popupDiv = null;
@@ -139,6 +157,25 @@ function closePopup(event){
     popup.style.display = 'none'
 }
 
+function renderMarkdown(input){
+    let markdown = marked.parse(input);
+    markdown = markdown.replace(/\$\$(.+?)\$\$/g, (match, latex) => {
+                try {
+                    return katex.renderToString(latex, { displayMode: true, throwOnError: false });
+                } catch (error) {
+                    return `<span style="color: red;">${error.message}</span>`;
+                }
+   });
+  markdown = markdown.replace(/\$(.+?)\$/g, (match, latex) => {
+                try {
+                    return katex.renderToString(latex, { displayMode: false, throwOnError: false });
+                } catch (error) {
+                    return `<span style="color: red;">${error.message}</span>`;
+                }
+   });
+   return markdown;
+}
+
 function renderDetails(data){
     if(!popup){
         console.error("something went wrong");
@@ -187,7 +224,7 @@ function renderDetails(data){
     html += `<div class="response-block">
             <h3 class="response-field">Your input:</h3>
 	    <br>
-            <p class="response-field">${data.user_input}</p>
+            <p class="response-field">${renderMarkdown(data.user_input)}</p>
         </div>`
     popupDiv.innerHTML = html
     try {
